5d1a496f171f9768dd19445fd724e679f37d0bd8
Vincent <2039855@carrefour.cegepvicto.ca>
2022-01-29 12:43:50 -0500

    Corriger le code, boucle de 0 à 10 pour inserer dans le tableau et remplacer le b* par b dans la fonction

--- prog3_tracer_debugger.cpp
+++ prog3_tracer_debugger.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <random>
+#include "prog3_tracer_debugger.h"
 
 using namespace std;
 
 void maFunc(int* a, int b) 
 {
 	int temp = *a;
-	*a = *b;
-	*b = temp;
+	*a = b;
+	b = temp;
 }
 
 int main()
@@ -15,11 +16,11 @@ int main()
 	std::random_device rd;
 	std::mt19937 rn(rd());
 	// dist1 sert à normaliser le nombre généré entre 1 et 100
-	std::uniform_int_distribution<> rng(1, 100);
+	std::uniform_int_distribution<> rng(1, 99);
 
 	int tab[10]{};
 
-	for (int i = 0; i < 100; i++)
+	for (int i = 0; i < 10; i++)
 	{
 		tab[i] = rng(rd);
 		cout << tab[i] << " ";
